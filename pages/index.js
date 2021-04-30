9bdd47ccf5b8e53a2956b182c89e347af2fdf2f4
kostadubovskiy <kostadubovskiy@gmail.com>
2021-04-30 19:35:08 -0400

    remove sample site message

--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,6 @@ export default function Home({ allPostsData }) {
           here :D. Tweet at me!{' '}
           <a href="https://twitter.com/kostadubovskiy" target="_blank">Twitter</a>.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
