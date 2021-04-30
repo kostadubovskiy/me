d8f7f7bc63bf7803b1dbc2b6e460ea9bfaa65a04
kostadubovskiy <kostadubovskiy@gmail.com>
2021-04-29 22:24:36 -0400

    date -- comment out

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
-import Date from '../components/date'
+// import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
@@ -32,7 +32,7 @@ export default function Home({ allPostsData }) {
               </Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                //<Date dateString={date} />
               </small>
             </li>
           ))}
