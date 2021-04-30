803faf2e023f8d9786e15f36a82c6fff809ee428
kostadubovskiy <kostadubovskiy@gmail.com>
2021-04-30 19:01:32 -0400

    title

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Kosta Dubovskiy'  // my name
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Kosta Dubovskiy'
+export const siteTitle = 'Kosta Dubovskiy'
 
 export default function Layout({ children, home }) {
   return (
