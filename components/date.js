ddd0c81bad4dffe97694ac535ff8204e9c92df90
kostadubovskiy <kostadubovskiy@gmail.com>
2021-05-01 14:03:46 -0400

    datetime -> dateTime

--- components/date.js
+++ components/date.js
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
   let date = parseISO(dateString)
-  return <time datetime={dateString}>{format(date, 'MMMM d, yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, 'MMMM d, yyyy')}</time>
 }
