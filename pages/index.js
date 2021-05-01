27c6e51c28d2d243de56ca385d2a8dc7a1c2d04d
kostadubovskiy <kostadubovskiy@gmail.com>
2021-04-30 20:23:08 -0400

    bio

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,13 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>  // A bit about me
+      <section className={utilStyles.headingMd}>
         <p>
           Hey! I&#8217;m Kosta. I&#8217;m a highschool
-          sophomore from NYC. I&#8217;m speak English, Russian and some Spanish but
-          I was born in Geneva, Switzerland. I lived in Boston as well as
-          Palo Alto, California for 2 years each, I&#8217;ve been living in NYC
-          since 2010. I love math, computer science, philosophy, travel, music,
+          sophomore from NYC, and I speak English, Russian as well as some Spanish.
+          I was born in Geneva, Switzerland and I lived in Boston as well as
+          California for 2 years each and since then, I&#8217;ve been in NYC.
+          I love math, computer science, philosophy, travel, music,
           neuroscience, psychology, and more! I&#8217;m currently
           exploring web and app development, which might be how you ended up
           here :D.<br/> <br/>
