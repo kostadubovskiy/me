c388b39d8ab3b11e6bd2fad21a9b52f7d4ed3391
Kosta Dubovskiy <kosta@MacBook-Pro-5.lan>
2023-03-08 21:34:49 -0500

    yarn

--- pages/index.js
+++ pages/index.js
@@ -134,7 +134,7 @@ export default function Home({ allPostsData }) {
               Duis aute irure dolor in reprehenderit in voluptate velit
               esse cillum dolore eu fugiat nulla pariatur. Excepteur 
               sint occaecat cupidatat non proident, sunt in culpa 
-              qui officia deserunt, mollit anim id est laborum.
+              qui officia deserunt, mollit anim, id est laborum.
               </p>
             </div>
           </WorkPreview1>
